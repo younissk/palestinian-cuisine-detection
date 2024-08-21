0dcd83e35d61e6b37efa3edcd68c69cf25d4ebe9
kandah@abk.at <kandah@abk.at>
2024-08-21 09:27:11 +0200

    Some small changes to image collection

--- collect-images.py
+++ collect-images.py
@@ -3,6 +3,7 @@ import requests
 import pprint
 import time
 from urllib.parse import quote
+from google_images_search import GoogleImagesSearch
 
 
 from dotenv import load_dotenv
@@ -20,48 +21,25 @@ SAVE_FOLDER = "images/maqluba"
 # Create the directory if it doesn't exist
 if not os.path.exists(SAVE_FOLDER):
     os.makedirs(SAVE_FOLDER)
+    
+gis = GoogleImagesSearch(API_KEY, SEARCH_ENGINE_ID)
 
-# From Google: Custom Search JSON API provides 100 search queries per day for free
 def fetch_images(query, num_images, save_folder):
-    # Simplify the query
-    encoded_query = quote(query)
-
-    search_url = f"https://www.googleapis.com/customsearch/v1?q={encoded_query}&cx={SEARCH_ENGINE_ID}&key={API_KEY}&searchType=image&num={min(num_images, 10)}"
     
-    response = requests.get(search_url).json()
+    _search_params = {
+        'q': query,
+        'num': num_images,
+        'fileType': 'jpg',
+        'num': num_images,
+    }
     
-    if 'items' not in response:
-        print(f"No items found for {query}")
-        print(f"Search URL: {search_url}")
-        pprint.pprint(f"Response: {response}")
-        return
-    
-    for i, item in enumerate(response['items']):
-        img_url = item['link']
-        try:
-            img_data = requests.get(img_url).content
-            # Get the file extension
-            file_extension = os.path.splitext(img_url)[1]  
-            
-            # If there is no file extension, default to .jpg
-            if not file_extension:
-                file_extension = '.jpg'  
-                
-            # Save the image to the specified folder
-            with open(f'{save_folder}/{query.replace(" ", "_")}_{i+1}{file_extension}', 'wb') as img_file:
-                img_file.write(img_data)
-                
-            # Print the name of the image that was downloaded
-            print(f"Downloaded {query.replace(' ', '_')}_{i+1}{file_extension}")
-            
-        except Exception as e:
-            print(f"Could not download {img_url}. Error: {e}")
+    gis.search(search_params=_search_params, path_to_dir=save_folder)
 
 
 dishes = [
     "Musakhan", "Mansaf", "Maqluba", "Mandi",
     "Mulukhiyyah", "Fasoulya", "Bamia",
-    "Bamia", "Kofta", "Kebab Halabi",
+    "Kofta", "Kebab Halabi",
     "Shawarma", "Sumaghiyyeh", "Kibbeh",
     "Falafel", "Hummus", "Malfuf",
     "Waraq al-'anib", "Baba Ghanoush", "Galayt bandora",
@@ -81,4 +59,7 @@ for dish in dishes:
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
         
-    fetch_images(query, num_images_per_dish, save_folder)
+    try: 
+        fetch_images(query, num_images_per_dish, save_folder)
+    except:
+        continue
